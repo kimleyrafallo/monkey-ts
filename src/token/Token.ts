32b7dc9f863ceacb986d870b8e28156e24fcecc5
kimleyrafallo <earlkimleyrafallo@gmail.com>
2024-06-22 01:39:47 +0800

    Changed the Token to be a class instead of type

--- src/token/Token.ts
+++ src/token/Token.ts
@@ -1,6 +1,15 @@
 import { TokenType } from "./TokenType"
 
-export type Token = {
-  tokenType: TokenType,
-  literal: string | null
+export class Token {
+  tokenType: TokenType;
+  literal: string | null;
+
+  constructor(tokenType: TokenType, literal: string | null){
+    this.tokenType = tokenType;
+    this.literal = literal;
+  }
+
+  stringify(){
+    return `type: ${this.tokenType}, literal: ${this.literal}`;
+  }
 }
